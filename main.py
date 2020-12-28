e879a30922f1d9a8c6c43f7272d50e38525c521a
Sangharsh Seth <sethsangharsh@Outlook.com>
2020-12-28 16:30:31 +0530

    parsed response to print the peer ips

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import asyncio
-
-import parse
-import trackerequest
 import random
+import socket
+import struct
 from urllib.parse import urlencode
+import requests
+import bencoding
+import parse
 
 path = './torrents/ubuntu-20.04.1-desktop-amd64.iso.torrent'
 
@@ -28,7 +29,15 @@ query = {
 }
 
 url = (torrentMetaData['announce']).decode('utf-8') + "?" + urlencode(query)
-print(url)
-trackerequest.main('www.google.com')
-loop = asyncio.get_event_loop()
-loop.run_until_complete(trackerequest.main(url))
+print('Connecting to torrent tracker..........', url)
+res = requests.get(url)
+print(type(res.text), type(res.content))
+data = bencoding.Decoder(res.content).decode()
+peerlist = data[b'peers']
+print(len(peerlist))
+print(type(data[b'peers']), data)
+
+offset = 0
+ip1 = struct.unpack_from("!i", peerlist, offset)[0]
+firstip = socket.inet_ntoa(struct.pack("!i", ip1))
+print(firstip)
